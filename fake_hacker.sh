3136e8f622ac79925c9fd2670c4aa808f6424bc7
ToriK17 <vfcad@protonmail.com>
2020-12-23 09:23:27 -0700

    new repo

---
+++ fake_hacker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "hello, $USER. I wants the files on your computer."
+sleep 3 
+echo ". . ."
+sleep 2
+echo "ooh, these files look nice."
+ls $HOME
+ls ~/
+echo ". . ."
+echo "now they are mine!"
+echo "muahahaha!!!!!!"
+sleep 2
+echo "thank you!" 
