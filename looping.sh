374d9f33cf0cdb2967d8ee1d551f03a35329bd6d
ToriK17 <vfcad@protonmail.com>
2020-12-25 08:39:50 -0700

    more shell exercises

--- looping.sh
+++ looping.sh
@@ -8,4 +8,24 @@ done
 # Without the ; added above the numbers print out of order
 echo {1..99..2}
 
-x=99; for ((i=1; i<=x; i++)); do echo $i; done
+x=99; for ((i=1; i<=x; i+2)); do echo $i; done
+
+# use a for loop to display the natural numbers from 1 to 50.
+x=50; for ((i=1; i<=x; i++)); do echo $i; done
+
+#  For input x and y:
+# return one of the following:
+# X is greater than Y
+# X is less than Y
+#  X is equal to Y
+read x
+read y
+if [ $x -gt $y ]
+then echo "X is greater than Y"
+fi
+if [ $x -lt $y ]
+then echo "X is less than Y"
+fi
+if [ $x -eq $y ]
+then echo "X is equal to Y"
+fi
